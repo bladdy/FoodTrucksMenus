a3575e322edc7f0ca44805caf902dae3b426c4b1
Bladimir Almanzar <bladdy34@gmail.com>
2022-12-01 20:57:03 -0400

    Creando los demas cruds

---
+++ FoodTrucksMenus/FoodTrucksMenus/Models/CityViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FoodTrucksMenus.Models
+{
+    public class CityViewModel
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "Ciudad")]
+        [MaxLength(50, ErrorMessage = "El campo {0} debe tener maximo {1} caracteres.")]
+        [Required(ErrorMessage = "El campo {0} es obigatorio.")]
+        public string Name { get; set; }
+
+        public int StateId { get; set; }
+    }
+}
---
+++ FoodTrucksMenus/FoodTrucksMenus/Models/StateViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FoodTrucksMenus.Models
+{
+    public class StateViewModel
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "Provincia/Estado")]
+        [MaxLength(50, ErrorMessage = "El campo {0} debe tener maximo {1} caracteres.")]
+        [Required(ErrorMessage = "El campo {0} es obigatorio.")]
+        public string Name { get; set; }
+
+        public int CountryId { get; set; }
+    }
+}
