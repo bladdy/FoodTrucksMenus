952e85c76a7e23783fc14b6f684767f9cd4e357c
Bladimir Almanzar <bladdy34@gmail.com>
2022-12-07 06:44:04 -0400

    Modificacon vista productos

--- FoodTrucksMenus/FoodTrucksMenus/Models/EditProductViewModel.cs
+++ FoodTrucksMenus/FoodTrucksMenus/Models/EditProductViewModel.cs
@@ -12,7 +12,7 @@ namespace FoodTrucksMenus.Models
         [MaxLength(50, ErrorMessage = "El campo {0} debe tener máximo {1} caractéres.")]
         [Required(ErrorMessage = "El campo {0} es obligatorio.")]
         public string Name { get; set; }
-
+        [DataType(DataType.MultilineText)]
         [Display(Name = "Descripción")]
         [MaxLength(500, ErrorMessage = "El campo {0} debe tener máximo {1} caractéres.")]
         [Required(ErrorMessage = "El campo {0} es obligatorio.")]
