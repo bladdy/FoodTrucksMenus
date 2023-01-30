50a378dc60e963ac543487b316fd7072d8d72b9c
Bladimir Almanzar <bladdy34@gmail.com>
2023-01-30 16:36:49 -0400

    Controler table

---
+++ FoodTrucksMenus/FoodTrucksMenus/Controllers/TableController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FoodTrucksMenus.Controllers
+{
+    public class TableController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult CreateTable()
+        {
+            return View();
+        }
+    }
+}
